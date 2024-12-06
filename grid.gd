717407477db88ef08d6907770e606f3c4738ac0a
Pete <petersgiles@users.noreply.github.com>
2024-12-07 10:39:18 +1100

    grid and tile

---
+++ grid.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var grid_width: int = 10
+@export var grid_height: int = 10
+@export var tile_size: int = 64
+@export var tile_scene: PackedScene  # Drag your Tile.tscn scene here in the Inspector
+
+func _ready():
+	generate_grid()
+
+func generate_grid():
+	for x in range(grid_width):
+		for y in range(grid_height):
+			var tile = tile_scene.instantiate()
+			tile.position = Vector2(x * tile_size, y * tile_size)
+			add_child(tile)
